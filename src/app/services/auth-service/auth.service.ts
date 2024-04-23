90f4f55052e8ab801798076a838250975fa247a9
anonfedora <musaeleazar090@gmail.com>
2024-04-23 11:00:00 +0100

    Services: auth

---
+++ src/app/services/auth-service/auth.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map, tap, switchMap } from "rxjs/operators";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { Observable, of } from "rxjs";
+import { User } from "../../model/user.interface";
+
+export interface LoginForm {
+    email: string;
+    password: string;
+}
+
+export const JWT_NAME = "blog-token";
+
+@Injectable({
+    providedIn: "root"
+})
+export class AuthService {
+    constructor(
+        private httpClient: HttpClient,
+        private jwtHelper: JwtHelperService
+    ) {}
+
+    login(loginForm: LoginForm) {
+        return this.httpClient
+            .post<any>("/api/users/login", {
+                email: loginForm.email,
+                password: loginForm.password
+            })
+            .pipe(
+                map(token => {
+                    console.log("token" + token.access_token);
+                    localStorage.setItem(JWT_NAME, token.access_token);
+                    return token;
+                })
+            );
+    }
+
+    logout() {
+        localStorage.removeItem(JWT_NAME);
+    }
+
+    register(user: User) {
+        return this.httpClient.post<any>("/api/users", user);
+    }
+
+    isAuthenticated(): boolean {
+        const token = localStorage.getItem(JWT_NAME);
+        return !this.jwtHelper.isTokenExpired(token);
+    }
+    getUserId(): Observable<number> {
+        return of(localStorage.getItem(JWT_NAME)).pipe(
+            switchMap((jwt: string) =>
+                of(this.jwtHelper.decodeToken(jwt)).pipe(
+                    map((jwt: any) => jwt.user.id)
+                )
+            )
+        );
+    }
+}
