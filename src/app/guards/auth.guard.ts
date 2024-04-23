e5fbf3043e330e6fe28d2005e6372c896b2a10db
anonfedora <musaeleazar090@gmail.com>
2024-04-23 11:29:01 +0100

    Auth: intercept, guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import {
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    UrlTree,
+    Router
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "../services/auth-service/auth.service";
+
+@Injectable({
+    providedIn: "root"
+})
+export class AuthGuard implements CanActivate {
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {}
+
+    canActivate(): boolean {
+        if (!this.authService.isAuthenticated()) {
+            this.router.navigate(["login"]);
+            return false;
+        }
+        return true;
+    }
+}
