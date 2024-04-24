8f1fdfb21d8745fbbe5690da695211b7764ca01b
anonfedora <musaeleazar090@gmail.com>
2024-04-24 18:46:48 +0100

    Register

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "./services/auth-service/auth.service";
+
+@Component({
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+    title = "frontend";
+
+    constructor(
+        private router: Router,
+        private authService: AuthService
+    ) {}
+
+    navigateTo(value) {
+        this.router.navigate(["../", value]);
+    }
+
+    logout() {
+        this.authService.logout();
+    }
+}
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from "@angular/core";
+import {
+    FormGroup,
+    FormBuilder,
+    Validators,
+    AbstractControl,
+    ValidationErrors
+} from "@angular/forms";
+import { AuthService } from "src/app/services/auth-service/auth.service";
+import { Router } from "@angular/router";
+import { map } from "rxjs/operators";
+
+class CustomValidators {
+    static passwordContainsNumber(control: AbstractControl): ValidationErrors {
+        const regex = /\d/;
+
+        if (regex.test(control.value) && control.value !== null) {
+            return null;
+        } else {
+            return { passwordInvalid: true };
+        }
+    }
+
+    static passwordsMatch(control: AbstractControl): ValidationErrors {
+        const password = control.get("password").value;
+        const confirmPassword = control.get("confirmPassword").value;
+
+        if (
+            password === confirmPassword &&
+            password !== null &&
+            confirmPassword !== null
+        ) {
+            return null;
+        } else {
+            return { passwordsNotMatching: true };
+        }
+    }
+}
+
+@Component({
+    selector: "app-register",
+    templateUrl: "./register.component.html",
+    styleUrls: ["./register.component.scss"]
+})
+export class RegisterComponent implements OnInit {
+    registerForm: FormGroup;
+
+    constructor(
+        private authService: AuthenticationService,
+        private formBuilder: FormBuilder,
+        private router: Router
+    ) {}
+
+    ngOnInit(): void {
+        this.registerForm = this.formBuilder.group(
+            {
+                name: [null, [Validators.required]],
+                username: [null, [Validators.required]],
+                email: [
+                    null,
+                    [
+                        Validators.required,
+                        Validators.email,
+                        Validators.minLength(6)
+                    ]
+                ],
+                password: [
+                    null,
+                    [
+                        Validators.required,
+                        Validators.minLength(3),
+                        CustomValidators.passwordContainsNumber
+                    ]
+                ],
+                confirmPassword: [null, [Validators.required]]
+            },
+            {
+                validators: CustomValidators.passwordsMatch
+            }
+        );
+    }
+
+    onSubmit() {
+        if (this.registerForm.invalid) {
+            return;
+        }
+        this.authService
+            .register(this.registerForm.value)
+            .pipe(map(user => this.router.navigate(["login"])))
+            .subscribe();
+    }
+}
