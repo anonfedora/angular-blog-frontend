2786b268202b7728ad60a356e1e5d260508244d6
anonfedora <musaeleazar090@gmail.com>
2024-04-25 21:02:58 +0100

    Users

---
+++ src/app/components/user/users/users.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from "@angular/core";
+import {
+    UserData,
+    UserService
+} from "src/app/services/user-service/user.service";
+import { map, tap } from "rxjs/operators";
+import { PageEvent } from "@angular/material/paginator";
+import { Router, ActivatedRoute } from "@angular/router";
+
+@Component({
+    selector: "app-users",
+    templateUrl: "./users.component.html",
+    styleUrls: ["./users.component.scss"]
+})
+export class UsersComponent implements OnInit {
+    filterValue: string = null;
+    dataSource: UserData = null;
+    pageEvent: PageEvent;
+    displayedColumns: string[] = ["id", "name", "username", "email", "role"];
+
+    constructor(
+        private userService: UserService,
+        private router: Router,
+        private activatedRoute: ActivatedRoute
+    ) {}
+
+    ngOnInit(): void {
+        this.initDataSource();
+    }
+
+    initDataSource() {
+        this.userService
+            .findAll(1, 10)
+            .pipe(map((userData: UserData) => (this.dataSource = userData)))
+            .subscribe();
+    }
+
+    onPaginateChange(event: PageEvent) {
+        let page = event.pageIndex;
+        let size = event.pageSize;
+
+        if (this.filterValue == null) {
+            page = page + 1;
+            this.userService
+                .findAll(page, size)
+                .pipe(map((userData: UserData) => (this.dataSource = userData)))
+                .subscribe();
+        } else {
+            this.userService
+                .paginateByName(page, size, this.filterValue)
+                .pipe(map((userData: UserData) => (this.dataSource = userData)))
+                .subscribe();
+        }
+    }
+
+    findByName(username: string) {
+        this.userService
+            .paginateByName(0, 10, username)
+            .pipe(map((userData: UserData) => (this.dataSource = userData)))
+            .subscribe();
+    }
+
+    navigateToProfile(id) {
+        this.router.navigate(["./" + id], { relativeTo: this.activatedRoute });
+    }
+}
