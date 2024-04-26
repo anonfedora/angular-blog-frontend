9977ad3f22edde199814ed4439e6455e8aa83ae5
anonfedora <musaeleazar090@gmail.com>
2024-04-26 15:52:22 +0100

    User Profile

---
+++ src/app/components/user/update-user-profile/update-user-profile.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from "src/app/services/auth-service/auth.service";
+import { UserService } from "src/app/services/user-service/user.service";
+import { switchMap, tap, map, catchError } from "rxjs/operators";
+import {
+    HttpEventType,
+    HttpEvent,
+    HttpErrorResponse
+} from "@angular/common/http";
+import { of } from "rxjs";
+import { User } from "src/app/model/user.interface";
+import { WINDOW } from "src/app/window-token";
+import { Inject } from "@angular/core";
+
+export interface File {
+    data: any;
+    progress: number;
+    inProgress: boolean;
+}
+
+@Component({
+    selector: "app-update-user-profile",
+    templateUrl: "./update-user-profile.component.html",
+    styleUrls: ["./update-user-profile.component.scss"]
+})
+export class UpdateUserProfileComponent implements OnInit {
+    @ViewChild("fileUpload", { static: false })
+    fileUpload: ElementRef;
+
+    form: FormGroup;
+
+    origin = this.window.location.origin;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private userService: UserService,
+        @Inject(WINDOW) private window: Window
+    ) {}
+
+    ngOnInit(): void {
+        this.form = this.formBuilder.group({
+            id: [{ value: null, disabled: true }, [Validators.required]],
+            name: [null, [Validators.required]],
+            username: [null, [Validators.required]],
+            profileImage: [null]
+        });
+
+        this.authService
+            .getUserId()
+            .pipe(
+                switchMap((id: number) =>
+                    this.userService.findOne(id).pipe(
+                        tap((user: User) => {
+                            this.form.patchValue({
+                                id: user.id,
+                                name: user.name,
+                                username: user.username,
+                                profileImage: user.profileImage
+                            });
+                        })
+                    )
+                )
+            )
+            .subscribe();
+    }
+
+    onClick() {
+        const fileInput = this.fileUpload.nativeElement;
+        fileInput.click();
+        fileInput.onchange = () => {
+            this.file = {
+                data: fileInput.files[0],
+                inProgress: false,
+                progress: 0
+            };
+            this.fileUpload.nativeElement.value = "";
+            this.uploadFile();
+        };
+    }
+
+    uploadFile() {
+        const formData = new FormData();
+        formData.append("file", this.file.data);
+        this.file.inProgress = true;
+
+        this.userService
+            .uploadProfileImage(formData)
+            .pipe(
+                map(event => {
+                    switch (event.type) {
+                        case HttpEventType.UploadProgress:
+                            this.file.progress = Math.round(
+                                (event.loaded * 100) / event.total
+                            );
+                            break;
+                        case HttpEventType.Respnse:
+                            return event;
+                    }
+                }),
+                catchError((error: HttpErrorResponse) => {
+                    this.file.inProgress = false;
+                    return of("Upload failed");
+                })
+            )
+            .subscribe((event: any) => {
+                if (typeof event === "object") {
+                    this.form.patchValue({
+                        profileImage: event.body.profileImage
+                    });
+                }
+            });
+    }
+
+    update() {
+        this.userService.updateOne(this.form.getRawValue()).subscribe();
+    }
+}
